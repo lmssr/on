5dc806369e8f314de529cc5fece0480023708da3
Luc Mosser <luc.mosser86@orange.fr>
2020-12-09 16:21:42 +0100

    first commit

---
+++ linkedListExercise.py
@@ -0,0 +1,13 @@
+class Node:
+ # Function to initialise the node object 
+    def __init__(self, data): 
+        self.data = data  # Assign data 
+        self.next = None  # Initialize next as null 
+
+        # Linked List class 
+class LinkedList: 
+     
+    # Function to initialize the Linked  
+    # List object 
+    def __init__(self):  
+        self.head = None
